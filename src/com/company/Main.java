0f15a7ad384642196de5d9bcc3ed936682a7ed6e
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2020-11-16 14:19:55 +0400

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        if (Character.isUpperCase(a.charAt(0)))
-            System.out.println(a.toLowerCase());
-        else
-            System.out.println(a.toUpperCase());
+        int a, b;
+        for(a = 0; a <= 1; a++)
+            for(b=-1; b <=0; b++)
+                System.out.print((a == 0 || b == -1) + " ");
+        System.out.println();
+        for(a = 0; a <= 1; a++)
+            for(b=-1; b <=0; b++)
+                System.out.print((a != 0 && b != -1) + " ");
+        System.out.println();
+
     }
 }
 
