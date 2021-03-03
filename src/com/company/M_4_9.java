9a3aa26c28e3c91086c3bc2d9fab7a8fdf190892
yandex-lyceum-yakovlev <70xSw2okcpFa7ur>
2021-03-03 16:49:42 +0400

    M_4_9 red

--- src/com/company/M_4_9.java
+++ src/com/company/M_4_9.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class M_4_9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
