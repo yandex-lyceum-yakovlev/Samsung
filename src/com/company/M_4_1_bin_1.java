e3cdd9a10b0875e06b964992b52a69b3af19139a
yandex-lyceum-yakovlev <56777612+yandex-lyceum-yakovlev@users.noreply.github.com>
2021-04-07 18:05:37 +0400

    Update M_4_1_bin_1.java

--- src/com/company/M_4_1_bin_1.java
+++ src/com/company/M_4_1_bin_1.java
@@ -1,23 +1,24 @@
-package com.company;
-
 import java.util.Arrays;
 import java.util.Scanner;
+        public class Main {
 
-public class M_4_1_bin_1 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        long[] a = new long[n];
-        long[] b = new long[k];
-        for(int i = 0; i<n;i++) a[i] = in.nextLong();
-        for(int i = 0; i<k;i++) b[i] = in.nextLong();
-        for (long i:b){
-            if (Arrays.binarySearch(a, i)<0)
-                System.out.println("NO");
-            else
-                System.out.println("YES");
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                int n = in.nextInt();
+                int k = in.nextInt();
+                int[] a = new int[n];
+                for(int i = 0; i < n; i++){
+                    a[i] = in.nextInt();
+                }
+                Arrays.sort(a);
+                int[] b = new int[k];
+                for(int i = 0; i < k; i++){
+                    b[i] = in.nextInt();
+                }
+                for(int i = 0; i < k; i++){
+                    int retVal = Arrays.binarySearch(a, b[i]);
+                    if(retVal < 0)System.out.println("NO");
+                    else System.out.println("YES");
+                }
+            }
         }
-    }
-}
-
